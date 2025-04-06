9b46421298009f14a7950da5dd67c0ef5498e3fc
Tomoki Watanabe <tw20thcenturyboy@gmail.com>
2025-04-06 17:25:14 +0900

    Fix layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,13 @@ import ThemeWrapper from '@/components/ThemeWrapper'
 import { Toaster } from 'react-hot-toast'
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '/fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 })
+
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '/fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 })
